1bcb2bdbbe0e4de088e0f369008ef052c042c2b7
Dylan Ocampo <dylanomar@live.com.mx>
2024-12-12 15:17:45 -0600

    ui changes

--- Assets/Scripts/ReactController.cs
+++ Assets/Scripts/ReactController.cs
@@ -10,14 +10,17 @@ public class ReactController : MonoBehaviour
   private static extern void isLoadedF ();
 
     public RectTransform chatContainer;
-
+    public RectTransform suggestionBox;
 
     void Start(){
         CommunicateToReact();
+        AdjustSize(1);
     }
 
     public void AdjustSize(int num){
         Debug.Log("Adjusted Size");
+        suggestionBox.offsetMin = new Vector2(50, 91);
+        suggestionBox.offsetMax = new Vector2(-50, 0);
         chatContainer.offsetMin = new Vector2(200, 0);
         chatContainer.offsetMax = new Vector2(-200, -64);
     }
