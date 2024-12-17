1c33010515d58886293de1637673033c7cb0f0a3
Dylan Ocampo <dylanomar@live.com.mx>
2024-12-17 12:47:41 -0600

    wait Between Messages 2

--- Assets/Scripts/WebCalls.cs
+++ Assets/Scripts/WebCalls.cs
@@ -8,10 +8,19 @@ using System;
 
 public class WebCalls : MonoBehaviour
 {
+
+    [System.Serializable]
+    public class idRoot
+    {
+        public int id;
+    }
+
+
     public SuggestionBox suggestionBox;
     public DefaultMessageRoot jsonProcessed;
     private IEnumerator CR_GetDefaultMessages()
     {
+        Debug.Log("aqui");
         string jsonResult = "";
         string url = "http://165.232.151.217/api/v1/default-messages";
         using (UnityWebRequest webRequest = UnityWebRequest.Get(url))
@@ -42,24 +51,21 @@ public class WebCalls : MonoBehaviour
         StartCoroutine(CR_GetDefaultMessages());
     }
 
-    public void PostConversationStarted(string id){
+    public void PostConversationStarted(int id){
         StartCoroutine(CR_PostConversationStarted(id));
     }
 
-    private IEnumerator CR_PostConversationStarted(string id)
+    private IEnumerator CR_PostConversationStarted(int _id)
     {
         string jsonResult = "";
         string url = "http://165.232.151.217/api/v1/conversations/started";
 
-        string jsonBody = JsonUtility.ToJson(new { id = id });
-
-        using (UnityWebRequest webRequest = UnityWebRequest.PostWwwForm(url, "POST")) {
-            byte[] bodyRaw = Encoding.UTF8.GetBytes(jsonBody);
-            webRequest.uploadHandler = new UploadHandlerRaw(bodyRaw);
-            webRequest.SetRequestHeader("Content-Type", "application/json");
-            webRequest.downloadHandler = new DownloadHandlerBuffer();
+        string jsonBody = JsonUtility.ToJson(new idRoot { id = _id });
+        Debug.Log(jsonBody);
+        using (UnityWebRequest webRequest = UnityWebRequest.Post(url, jsonBody,"application/json")) {
+            webRequest.SetRequestHeader("Accept", "application/json");
             yield return webRequest.SendWebRequest();
-
+            Debug.Log(webRequest.result);
             if (webRequest.result == UnityWebRequest.Result.ProtocolError || webRequest.result == UnityWebRequest.Result.ConnectionError)
             {
                 Debug.Log("Protocol Error or Connection Error");
